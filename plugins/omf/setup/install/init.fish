4f830879e2842013f321d45e21fe23f76b636165
Eddy Ekofo <eddyecko@gmail.com>
2021-01-06 22:28:13 +0100

    Add oh-my-fish config

---
+++ plugins/omf/setup/install/init.fish
@@ -0,0 +1,5 @@
+# install Oh My Fish
+
+curl -L https://get.oh-my.fish > install &&
+fish install --path=~/.local/share/omf --config=~/.config/omf &&
+omf install https://github.com/jhillyerd/plugin-git bang-bang
