83686e8d1b28fce1809bf1c189894c6d85444256
Eddy Ekofo <eddyecko@gmail.com>
2021-01-10 05:11:59 +0100

    Improve fzf search, exculse .git & node_modules

--- plugins/fzf/lib/interactive/fiz.fish
+++ plugins/fzf/lib/interactive/fiz.fish
@@ -1,1 +1,3 @@
 source $HOME/.config/fish/functions/fish_user_key_bindings.fish
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs -g "!{node_modules,.git}" | fzf'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
