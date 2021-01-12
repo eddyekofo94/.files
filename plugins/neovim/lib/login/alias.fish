c1a6c784e2bcdb4924194bf97949f754fe3e4b57
Eddy Ekofo <eddyecko@gmail.com>
2021-01-12 06:51:19 +0100

    Fix neovim setup

--- plugins/neovim/lib/login/alias.fish
+++ plugins/neovim/lib/login/alias.fish
@@ -1,7 +1,8 @@
 set NVIM_PERSONAL_DIR "$DOT_PLUGINS_DIR/neovim/nvim"
-set NVIM_PERSONAL_INIT "$NVIM_PERSONAL_DIR/init.vim"
+set NVIM_PERSONAL_INIT "$NVIM_PERSONAL_DIR/init.lua"
+set MYVIMRC "$XDG_CONFIG_HOME/nvim/init.vim"
 
 if test -e $NVIM_PERSONAL_INIT
     alias nvim "nvim -u $NVIM_PERSONAL_INIT"
-    alias vim "nvim -u $XDG_CONFIG_HOME/nvim/init.vim"
+    alias vim "nvim -u $MYVIMRC"
 end
