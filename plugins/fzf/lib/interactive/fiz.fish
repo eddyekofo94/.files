99e76217739bc51c13174802217ad4ce5376cb11
Eddy Ekofo <eddyecko@gmail.com>
2021-01-12 06:52:04 +0100

    Fix fzf

--- plugins/fzf/lib/interactive/fiz.fish
+++ plugins/fzf/lib/interactive/fiz.fish
@@ -1,3 +1,9 @@
 source $HOME/.config/fish/functions/fish_user_key_bindings.fish
-export FZF_DEFAULT_COMMAND='rg --files --hidden'
+# --files: List files that would be searched but do not search
+# --no-ignore: Do not respect .gitignore, etc...
+# --hidden: Search hidden files and folders
+# --follow: Follow symlinks
+# --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+# export FZF_DEFAULT_COMMAND='rg --files --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
