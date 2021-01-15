b38979dce1a22a7f83bdce55c356c8bff261eed0
Eddy Ekofo <eddyecko@gmail.com>
2021-01-15 01:52:52 +0100

    Some litte improvements

---
+++ plugins/terminal/aliases/fish/up.fish
@@ -0,0 +1,16 @@
+# TODO: Adapt to fish shell
+# function up
+#     if test "$args" <1
+#         command cd ..
+#         # else
+#         #     set CDSTR ""
+#         #     for i in (seq )
+#         #         set CDSTR "../$CDSTR"
+#         #         command cd $CDSTR
+#         #     end
+#     end
+# end
+
+# for CHAR in (seq $COLUMNS)
+#     printf $argv[1]
+# end
