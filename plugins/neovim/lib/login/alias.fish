e420a3f75bb33cfa8ca3f05070369a95c504ab0f
Eddy Ekofo <eddyecko@gmail.com>
2021-01-15 01:49:41 +0100

    Neovim improvements

--- plugins/neovim/lib/login/alias.fish
+++ plugins/neovim/lib/login/alias.fish
@@ -1,8 +1,8 @@
-set NVIM_PERSONAL_DIR "$DOT_PLUGINS_DIR/neovim/nvim"
-set NVIM_PERSONAL_INIT "$NVIM_PERSONAL_DIR/ginit.vim"
-set MYVIMRC "$XDG_CONFIG_HOME/nvim/init.vim"
+#set NVIM_PERSONAL_DIR "$DOT_PLUGINS_DIR/neovim/nvim"
+#set NVIM_PERSONAL_INIT "$NVIM_PERSONAL_DIR/ginit.vim"
+#set MYVIMRC "$XDG_CONFIG_HOME/nvim/init.vim"
 
-if test -e $NVIM_PERSONAL_INIT
-    alias nvim "nvim -u $NVIM_PERSONAL_INIT"
-    alias vim "nvim -u $MYVIMRC"
-end
+#if test -e $NVIM_PERSONAL_INIT
+#    alias nvim "nvim -u $NVIM_PERSONAL_INIT"
+#    alias vim "nvim -u $MYVIMRC"
+#end
