eaed8eaccf8ee91f72805a8eb5e0b9a21f45e48b
Eddy Ekofo <eddyecko@gmail.com>
2021-04-28 17:35:29 +0100

    Change colour to Tokyonight

--- plugins/fish/lib/login/init.fish
+++ plugins/fish/lib/login/init.fish
@@ -1,25 +1,59 @@
 # Fish syntax highlighting
-set -g fish_color_autosuggestion 'aaa' 'brblack'
-set -g fish_color_cancel -r
-set -g fish_color_command 'green' --bold
-set -g fish_color_comment red
-set -g fish_color_cwd green
-set -g fish_color_cwd_root red
-set -g fish_color_end brmagenta
-set -g fish_color_error brred
-set -g fish_color_escape 'bryellow' '--bold'
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator brorange
-set -g fish_color_param white
-set -g fish_color_quote bryellow
-set -g fish_color_redirection brblue
-set -g fish_color_search_match 'bryellow' '--background=brblack'
-set -g fish_color_selection 'white' '--bold' '--background=brblack'
-set -g fish_color_user brgreen
-set -g fish_color_valid_path --underline
+# set -g fish_color_autosuggestion 'aaa' 'brblack'
+# set -g fish_color_cancel -r
+# set -g fish_color_command 'green' --bold
+# set -g fish_color_comment red
+# set -g fish_color_cwd green
+# set -g fish_color_cwd_root red
+# set -g fish_color_end brmagenta
+# set -g fish_color_error brred
+# set -g fish_color_escape 'bryellow' '--bold'
+# set -g fish_color_history_current --bold
+# set -g fish_color_host normal
+# set -g fish_color_match --background=brblue
+# set -g fish_color_normal normal
+# set -g fish_color_operator brorange
+# set -g fish_color_param white
+# set -g fish_color_quote bryellow
+# set -g fish_color_redirection brblue
+# set -g fish_color_search_match 'bryellow' '--background=brblack'
+# set -g fish_color_selection 'white' '--bold' '--background=brblack'
+# set -g fish_color_user brgreen
+# set -g fish_color_valid_path --underline
+
+# TokyoNight Color Palette
+set -l foreground c0caf5
+set -l selection 364A82
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
 
 set -x SHELL /usr/local/bin/fish
 
