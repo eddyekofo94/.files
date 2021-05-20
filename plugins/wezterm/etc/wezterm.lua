db2a37dc4bec86ab1b6232f421324a8c4f8a42e9
Eddy Ekofo <eddyecko@gmail.com>
2021-05-20 21:01:06 +0100

    Add wezterm terminal plugin

---
+++ plugins/wezterm/etc/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+return {
+    font = wezterm.font("UbuntuMono Nerd Font Mono"),
+    color_scheme = "TokyoNight",
+    font_size = 20.0,
+    hide_tab_bar_if_only_one_tab = true,
+    colors_scheme_dirs = {"$HOME/.config/wezterm/colors"}
+}
