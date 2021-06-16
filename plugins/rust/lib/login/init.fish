e21c52a361d8963594c7393d78c4c64355280d2f
Eddy Ekofo <eddyecko@gmail.com>
2021-06-16 15:11:26 +0100

    Add cargo abbr to fish shell

--- plugins/rust/lib/login/init.fish
+++ plugins/rust/lib/login/init.fish
@@ -1,2 +1,9 @@
 sh $HOME/.cargo/env
 abbr --add cg 'cargo'
+abbr --add cgi 'cargo install'
+abbr --add cgu 'cargo uninstall'
+abbr --add cgs 'cargo search'
+abbr --add cgn 'cargo new'
+abbr --add cgug 'cargo upgrade'
+abbr --add cgc 'cargo clean'
+abbr --add cgt 'cargo test'
