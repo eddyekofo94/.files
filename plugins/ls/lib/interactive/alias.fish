4c0b2df7459a80503036e383cf8f4b1db0caa52c
Eddy Ekofo <eddyecko@gmail.com>
2021-11-07 15:03:54 +0000

    Improve ls config, added vivid

--- plugins/ls/lib/interactive/alias.fish
+++ plugins/ls/lib/interactive/alias.fish
@@ -1,6 +1,9 @@
 # Colorize `ls` output using dircolors settings
 #set -g LS_COLORS "*.xyz=f0;50"
 
+
 alias ls="lsd"
 alias l="exa --group-directories-first --icons --long --header --binary --group"
 alias la="l -a"
+
+expot LS_COLORS=(vivid generate one-dark)
--- plugins/ls/lib/login/dircolors.fish
+++ plugins/ls/lib/login/dircolors.fish
@@ -1,2 +1,8 @@
 # Set LS_COLORS environment variable so `ls` knows how to color directories/files
 load_env (gdircolors $DOT_PLUGIN_DIR/etc/dircolors | psub)
+
+alias ls="lsd"
+alias l="exa --group-directories-first --icons --long --header --binary --group"
+alias la="l -a"
+
+expot LS_COLORS=(vivid generate one-dark)
