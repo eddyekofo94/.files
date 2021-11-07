cde68f43b6f4db2637b864df2fe3cbfb0f8a74cd
Eddy Ekofo <eddyecko@gmail.com>
2021-11-07 05:21:34 +0000

    Change ls config

--- plugins/ls/lib/interactive/alias.fish
+++ plugins/ls/lib/interactive/alias.fish
@@ -1,5 +1,6 @@
 # Colorize `ls` output using dircolors settings
 #set -g LS_COLORS "*.xyz=f0;50"
 
-alias l="exa --group-directories-first --long --header --binary --group"
+alias ls="lsd"
+alias l="exa --group-directories-first --icons --long --header --binary --group"
 alias la="l -a"
